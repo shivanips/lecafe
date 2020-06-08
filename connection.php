753a4794c2ad970e9b8360a697fec728e561f7c8
shivanips <66565913+shivanips@users.noreply.github.com>
2020-06-08 12:14:48 +0530

    Add files via upload

--- connection.php
+++ connection.php
@@ -3,19 +3,13 @@
 function Connect()
 {
 	$dbhost = "localhost";
-	$dbuser = "debian-sys-maint";
-	$dbpass = "5hKR2pnjQ7O5RxF9";
-	$dbname = "online";
+	$dbuser = "root";
+	$dbpass = "US9RXzLb92jGKe";
+	$dbname = "lecafe";
 
 	//Create Connection
 	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
-	if($conn){
-		echo "Datbase Connected";
-	}
-	else
-	{
-		echo "Error DB ";
-	}
+
 	return $conn;
 }
 ?>
